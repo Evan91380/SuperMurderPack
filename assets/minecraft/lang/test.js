2b0e6cc911db25e57c4cadfd5b07fa056d96f318
Evan91380 <evanpetit5@gmail.com>
2023-06-05 22:30:21 +0200

    text specta

--- assets/minecraft/lang/test.js
+++ assets/minecraft/lang/test.js
@@ -1,13 +1,17 @@
 data = require("./lang.json")
 const fs = require("fs")
 
-let en = {}
+let en = {	
+"Don't forget the ressource pack!": " ",
+"Click here to download the ressource pack!": " ",
+}
 let fr = {}
 const regexReturnLine = /\\n/g
 for(let key in data) {
     if(data[key]["en"] !== null) en[key.replace(regexReturnLine, "\n")] = data[key]["en"].replace(regexReturnLine, "\n")
     if(data[key]["fr"] !== null) fr[key] = data[key]["fr"].replace(regexReturnLine, "\n")
 }
+
 
 fs.writeFileSync(`./en_us.json`, JSON.stringify(en, null, '\t'))
 fs.writeFileSync(`./fr_fr.json`, JSON.stringify(fr, null, '\t'))
